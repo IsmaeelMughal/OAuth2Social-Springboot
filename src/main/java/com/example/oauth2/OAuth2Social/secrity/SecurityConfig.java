678727ac622e05881ab1e78bd3dca7a0af6a47bc
IsmaeelMughal <ismaeel.mughal.1912@gmail.com>
2023-10-18 13:38:19 +0500

    Initial commit

---
+++ src/main/java/com/example/oauth2/OAuth2Social/secrity/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.oauth2.OAuth2Social.secrity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                {
+                    authorizationManagerRequestMatcherRegistry
+                            .anyRequest()
+                            .authenticated();
+                })
+                .oauth2Login(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+}
